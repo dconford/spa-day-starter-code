a23721ace301db730a6afe89979e0d3ca7bafde3
Daniel C <daniel@daniel.com>
2020-01-15 15:54:54 -0600

    finished studio and bonus, user-signup-pt2.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,45 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Console;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    @Email(message="email format is incorrect, please check your entry")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Passwords not matching")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    public void checkPassword() {
+        if (getPassword() != null && getVerifyPassword() != null) {
+            if (!getPassword().contentEquals(getVerifyPassword())) {
+                setVerifyPassword(null);
+            }
+        }
     }
 
     public String getUsername() {
@@ -36,7 +62,17 @@ public class User {
         return password;
     }
 
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
 }
